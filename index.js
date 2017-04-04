f35c13715a41bc8d1c0ab6d34f0c236afac94405
zoeyyang123 <zoeyyang_bupt@163.com>
2017-04-04 23:31:03 +0800

    Add files via upload
    first change

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zoey on 17-4-3.
+ */
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);

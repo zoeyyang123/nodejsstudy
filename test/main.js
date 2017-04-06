c250e3d34d382b75eaeb78a8d3d89821c536f6a4
zoey <yangzhaoyi@minshenglife.com>
2017-04-06 18:06:27 +0800

    eventemitter done.

--- test/main.js
+++ test/main.js
@@ -1,6 +1,10 @@
 var fs = require("fs");
 
-var data = fs.readFileSync('input.txt');
-
-console.log(data.toString());
-console.log("程序执行结束!");
+fs.readFile('input.txt', (err, data) => {
+   if (err){
+      console.log(err.stack);
+      return;
+   }
+   console.log(data.toString());
+});
+console.log("程序执行完毕");
